44d4de2620f133052b35ae85345f5bdf3551e062
Chris George <cgeorge110@gmail.com>
2014-01-03 18:20:27 -0800

    Create User Model and Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Trip::Application.routes.draw do
+  resources :users
+  match "/signup", to: 'users#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
