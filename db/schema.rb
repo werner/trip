1acb86c8378e2516c6581e42b08219253e8f0e74
Werner <werner_a_e@yahoo.es>
2014-01-14 12:02:18 -0430

    use github with mercury gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140107055154) do
+ActiveRecord::Schema.define(version: 20140111100705) do
 
   create_table "articles", force: true do |t|
     t.integer  "user_id"
@@ -23,6 +23,22 @@ ActiveRecord::Schema.define(version: 20140107055154) do
     t.integer  "hero_file_size"
     t.datetime "hero_updated_at"
   end
+
+  create_table "ckeditor_assets", force: true do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.integer  "assetable_id"
+    t.string   "assetable_type",    limit: 30
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+  add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
 
   create_table "users", force: true do |t|
     t.string   "first_name"
